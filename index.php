70f548d25ba37888ee788cbeb8bba1f2e0055d34
Capwell <admin@axerius.net>
2019-02-27 16:09:39 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+	include '/php/head-start.php';
+?>
+<title>Landscape Lifestyles - Fencing, Paving & Planters and more! - Professional Landscapers</title>
+<?php
+	include '/php/head-end.php';
+?>
+
+<?php
+	include '/php/body-navigation.php';
+?>
+
+<!-- main content here -->
+
+
+<?php
+	include '/php/footer-end.php';
+?>
